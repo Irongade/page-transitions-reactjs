f3bf0f47ceb2d7ce90c768d6c567d54581aca721
Irongade <adeoluayangade@yahoo.com>
2021-04-22 00:13:35 +0100

    Page transitions with react, initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {AnimatePresence} from "framer-motion"
+//Pages
+import Home from "./pages/Home";
+import Model from "./pages/Model";
+//components
+import Header from "./components/Header";
+//Styles
+import "./App.scss";
+  
+const imageDetails = {
+  width: 324,
+  height: 400,
+};
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+    <Header />
+    <Route
+      render={({ location }) => (
+        <AnimatePresence initial={false} exitBeforeEnter>
+          <Switch location={location} key={location.pathname}>
+            <Route
+              exact
+              path='/'
+              render={() => <Home imageDetails={imageDetails} />}
+            />
+            <Route
+              exact
+              path='/model/:id'
+              render={() => <Model imageDetails={imageDetails} />}
+            />
+          </Switch>
+          </AnimatePresence>
+      )}
+    />
+  </Router>
+  )
 }
 
 export default App;
